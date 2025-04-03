0a8a155024b2b0345bfebfc8396fc41f4ec6d57d
João Pedro Soeiro Lemos <joaopedro@tec-adm.com.br>
2025-04-03 16:02:25 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FaSearch } from "react-icons/fa";
+import api from "./services/api";
+import validarCep from './services/regex';
+import './style.css';
+import Swal from 'sweetalert2';
 
 function App() {
+
+const [input, setInput] = useState('')
+const [cepData, setCepData] = useState(null);
+
+async function handleSearch(){
+  
+  if(!validarCep(input)){
+    Swal.fire({
+      title: 'Insira um CEP ou verifique se está correto.',
+      icon: 'error',
+    });
+    setCepData(null);
+    return;
+  }
+
+  try {
+    const response = await api.get(`${input}/json/`);
+    setCepData(response.data);
+    setInput('');
+
+  }catch(error){
+    alert("Erro ao buscar dados");
+    setInput('');
+    setCepData(null)
+  }
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="formulario">
+        <h1 className="buscador">Buscador</h1>
+        <div className="containerInput">
+          <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          />
+          <button className="buttonSearch" onClick={handleSearch}>
+            <FaSearch size={30} color="#000"/>
+          </button>
+        </div>
+
+
+        {cepData && (
+          <main>
+            <h2>CEP: {cepData.cep}</h2>
+            <p>Rua: {cepData.logradouro}, {cepData.siafi}</p>
+            <p>Bairro: {cepData.bairro}</p>
+            <p>Cidade: {cepData.localidade} - {cepData.uf}</p>
+          </main>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+
